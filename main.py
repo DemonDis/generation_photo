dc080c1fbfe6c12ac8d3b7f0d57be87100a10c82
Dmitry <50297797+DemonDis@users.noreply.github.com>
2025-12-05 15:35:53 +0700

    feat(main): update import path handling to support src as a subdirectory

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from src.app import run_app
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+from app import run_app
 
 if __name__ == '__main__':
     run_app()
